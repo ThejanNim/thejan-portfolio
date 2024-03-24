6a755ef80c4f245cbf4cda3f56bd82a229c05fe5
ThejanNim <thejannims@gmail.com>
2024-03-24 16:17:51 +0530

    Add new two font family to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        incognito: ['var(--incognito)'],
+        inter: ['var(--inter)']
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
